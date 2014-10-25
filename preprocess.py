9a6575e2dc235727bae791ee2ac051d73f13fe98
Chang-Min Oh <chmn.oh@gmail.com>
2014-10-26 00:40:08 +0900

    fix preprocessing bug.

--- preprocess.py
+++ preprocess.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python
-# -*- encoding: latin-1 -*-
+# -*- encoding: utf-8 -*-
 
 # convert multi-line "REMARK" field into single line.
 
-fp_in = open('data/StormDB.csv', 'rb')
-fp_out = open('data/StormDB2.csv', 'w')
+fp_in = open('data/StormDB-unzipped.csv', 'rb')
+fp_out = open('data/StormDB.csv', 'wb')
 
 nrow = 0
 for l in fp_in: # header
-	fp_out.write(l)
+	fp_out.write(l.replace("\r",""))
 	nrow += 1
 	break
 
 cnt = 0
 buf = []
-for l in fp_in:
-	line = ""
-	#print repr(l[-5:])
-	if l[-5:] == ".00\r\n":
-		if len(buf) > 0:
-			buf.append(l)
-			line = ''.join(buf)
-			buf = []
-			#print "merged line: ", line
+line_no = 2
+for line in fp_in:
+	last_elem = line.split(',')[-1].strip().split('.')
+	if len(last_elem) == 2 and last_elem[0].isalnum() and last_elem[1] == '00':
+		nrow += 1
+		data = ""
+		if len(buf) == 0:
+			data = line
 		else:
-			line = l
+			buf.append(line)
+			data = ''.join(buf)
+			buf = []
+		fp_out.write(data.replace("\r","").replace('""',''))
 	else:
 		if len(buf) == 0:
 			cnt += 1
-			#print nrow, "fail"
-		buf.append(l.replace("\r","").replace("\n",""))
-		continue
-	fp_out.write(line)
-	nrow += 1
-	#if nrow > 10:
-		#break
+		buf.append(line.strip())
+		#if cnt % 1000 == 0:
+		#	print line_no, "fmt err (%d)" % nrow
+	line_no += 1
 
 fp_in.close()
 fp_out.close()
 
-print "total failed line: %d/%d" % (cnt,nrow)
+#print "total format error line: %d/%d" % (cnt,nrow)
